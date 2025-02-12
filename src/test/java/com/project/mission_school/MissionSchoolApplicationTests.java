2bd82ae5dbfa87183dcc1c5ff1682f07665acc41
beejh02 <joon0448@naver.com>
2025-02-12 20:00:26 +0900

    프로젝트 초기 설정(#1)

---
+++ src/test/java/com/project/mission_school/MissionSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.mission_school;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MissionSchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
