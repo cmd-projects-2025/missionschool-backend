483f149cdcece4a9c78a51139f7b1798d66c3709
m222zu <147124424+m222zu@users.noreply.github.com>
2025-02-20 22:30:34 +0900

    [feat] Dto값 추가

--- src/main/java/com/project/mission_school/bulletin/dto/BulletinBoardDto.java
+++ src/main/java/com/project/mission_school/bulletin/dto/BulletinBoardDto.java
@@ -1,4 +1,19 @@
 package com.project.mission_school.bulletin.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
 public class BulletinBoardDto {
+    private String writerId;
+    private Long price;
+    private String title;
+    private String description;
+    private Boolean bulletinState;
+    private Integer viewCnt;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
--- src/main/java/com/project/mission_school/bulletin/entity/BulletinBoard.java
+++ src/main/java/com/project/mission_school/bulletin/entity/BulletinBoard.java
@@ -15,7 +15,6 @@ public class BulletinBoard {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-
     private Long id;
 
     private String writerId;
