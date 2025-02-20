eaee742cbc1ab5513ab683bfef9bed0260eaac73
beejh02 <joon0448@naver.com>
2025-02-20 20:45:05 +0900

    [feat] 회원가입 : LogoutController 작성

---
+++ src/main/java/com/project/mission_school/auth/controller/LogoutController.java
@@ -0,0 +1,27 @@
+package com.project.mission_school.auth.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+
+@RestController
+@RequestMapping("/api/user")
+public class LogoutController {
+
+    @PutMapping("/logout")
+    public ResponseEntity<String> logout(HttpServletResponse response) {
+        Cookie jwtCookie = new Cookie("jwt", null);
+        jwtCookie.setHttpOnly(true);
+        jwtCookie.setSecure(true);
+        jwtCookie.setPath("/");
+        jwtCookie.setMaxAge(0);
+
+        response.addCookie(jwtCookie);
+
+        return ResponseEntity.ok("로그아웃 성공");
+    }
+}
