2dd0b2538d2ed9fecf0a058ac9243748066a0a55
beejh02 <joon0448@naver.com>
2025-02-20 16:17:52 +0900

    [feat] 회원가입 : User Dto 작성

---
+++ src/main/java/com/project/mission_school/auth/dto/SignupRequest.java
@@ -0,0 +1,15 @@
+package com.project.mission_school.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class SignupRequest {
+    private String username;
+    private String password;
+}
