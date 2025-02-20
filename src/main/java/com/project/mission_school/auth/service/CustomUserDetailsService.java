14eb42dc0cdf22086e8302b9b7361ca064b54737
beejh02 <joon0448@naver.com>
2025-02-20 20:02:32 +0900

    [feat] 회원가입 : userdetails service 사용

--- src/main/java/com/project/mission_school/auth/service/CustomUserDetailsService.java
+++ src/main/java/com/project/mission_school/auth/service/CustomUserDetailsService.java
@@ -1,22 +1,22 @@
-// package com.project.mission_school.auth.service;
+package com.project.mission_school.auth.service;
 
-// import org.springframework.security.core.userdetails.UserDetails;
-// import org.springframework.security.core.userdetails.UserDetailsService;
-// import org.springframework.security.core.userdetails.UsernameNotFoundException;
-// import org.springframework.stereotype.Service;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
-// import com.project.mission_school.auth.repository.UserRepository;
+import com.project.mission_school.auth.repository.UserRepository;
 
-// import lombok.RequiredArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
-// @Service
-// @RequiredArgsConstructor
-// public class CustomUserDetailsService implements UserDetailsService {
-//     private final UserRepository userRepository;
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
 
-//     @Override
-//     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-//         return userRepository.findByUsername(username)
-//             .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
-//     }
-// }
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username)
+            .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
+    }
+}
