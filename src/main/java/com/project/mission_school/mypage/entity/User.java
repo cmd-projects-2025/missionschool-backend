5f7f49aa3f03ec82dbab31b636e970cee14e78e8
beejh02 <joon0448@naver.com>
2025-02-21 15:57:41 +0900

    [fix] 마이페이지 : UserEntity Setter 메서드 추가, 이상검사 삭제

--- src/main/java/com/project/mission_school/mypage/entity/User.java
+++ src/main/java/com/project/mission_school/mypage/entity/User.java
@@ -48,23 +48,15 @@ public class User implements UserDetails {
         return List.of(new SimpleGrantedAuthority(role));
     }
 
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
+    public void setUsername(String username) {
+        this.username = username;
     }
 
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
+    public void setPassword(String password) {
+        this.password = password;
     }
 
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
+    public void setPhonenumber(String phonenumber) {
+        this.phonenumber = phonenumber;
     }
 }
