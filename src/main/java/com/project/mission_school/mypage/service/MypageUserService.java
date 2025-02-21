af77b5a6981fb60b9ea36b8f1c17c6f37dec3d74
beejh02 <joon0448@naver.com>
2025-02-21 16:26:14 +0900

    [refactor] 마이페이지 : 파일명 변경으로 인한 UserService 리팩토링

--- src/main/java/com/project/mission_school/mypage/service/MypageUserService.java
+++ src/main/java/com/project/mission_school/mypage/service/MypageUserService.java
@@ -4,43 +4,43 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.project.mission_school.mypage.entity.User;
-import com.project.mission_school.mypage.repository.UserRepository;
+import com.project.mission_school.mypage.repository.MypageUserRepository;
+import com.project.mission_school.mypage.entity.MypageUser;
 
 @Service
-public class UserService {
-    private final UserRepository userRepository;
+public class MypageUserService {
+    private final MypageUserRepository mypageUserRepository;
     private final BCryptPasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
-        this.userRepository = userRepository;
+    public MypageUserService(MypageUserRepository mypageUserRepository, BCryptPasswordEncoder passwordEncoder) {
+        this.mypageUserRepository = mypageUserRepository;
         this.passwordEncoder = passwordEncoder;
     }
 
     @Transactional
-    public void updateUserInfo(Long userId, String username, String password, String phonenumber) {
-        User user = userRepository.findById(userId)
+    public void updateUserInfo(Long id, String username, String password, String phonenumber) {
+        MypageUser mypageUser = mypageUserRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("사용자를 찾을 수 없습니다."));
 
         if (username != null && !username.isEmpty()) {
-            user.setUsername(username);
+            mypageUser.setUsername(username);
         }
 
         if (password != null && !password.isEmpty()) {
-            user.setPassword(passwordEncoder.encode(password));
+            mypageUser.setPassword(passwordEncoder.encode(password));
         }
 
         if (phonenumber != null && !phonenumber.isEmpty()) {
-            user.setPhonenumber(phonenumber);
+            mypageUser.setPhonenumber(phonenumber);
         }
         
     }
 
     @Transactional
     public void deleteUser(Long userId) {
-        User user = userRepository.findById(userId)
+        MypageUser user = mypageUserRepository.findById(userId)
                 .orElseThrow(() -> new IllegalArgumentException("사용자를 찾을 수 없습니다."));
         
-        userRepository.delete(user);
+        mypageUserRepository.delete(user);
     }
 }
