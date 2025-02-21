313ce83bc68977d1f5479b83ca853219f8040b4e
beejh02 <joon0448@naver.com>
2025-02-21 16:40:16 +0900

    [feat] 마이페이지 : MypageMainController 작성

---
+++ src/main/java/com/project/mission_school/mypage/controller/MypageMainController.java
@@ -0,0 +1,33 @@
+package com.project.mission_school.mypage.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.mission_school.mypage.entity.MypageUser;
+import com.project.mission_school.mypage.service.MypageUserService;
+
+@RestController
+@RequestMapping("/api/user")
+public class MypageMainController {
+    private final MypageUserService mypageUserService;
+
+    public MypageMainController(MypageUserService mypageUserService) {
+        this.mypageUserService = mypageUserService;
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<MypageUser> getCurrentUser(Authentication authentication) {
+        if (authentication == null || !authentication.isAuthenticated()) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
+        String username = authentication.getName();
+        MypageUser user = mypageUserService.findByUsername(username);
+
+        return ResponseEntity.ok(user);
+    }
+}
