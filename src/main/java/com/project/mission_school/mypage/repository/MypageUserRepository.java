f67f0b5794b541ca721d61251ff996ff9e66cecf
beejh02 <joon0448@naver.com>
2025-02-21 16:25:24 +0900

    [refactor] 마이페이지 : 파일명 변경으로 인한 UserEntity 리팩토링

--- src/main/java/com/project/mission_school/mypage/repository/MypageUserRepository.java
+++ src/main/java/com/project/mission_school/mypage/repository/MypageUserRepository.java
@@ -5,9 +5,9 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.project.mission_school.mypage.entity.User;
+import com.project.mission_school.mypage.entity.MypageUser;
 
 @Repository
-public interface MypageUserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByUsername(String username);
+public interface MypageUserRepository extends JpaRepository<MypageUser, Long> {
+    Optional<MypageUser> findByUsername(String username);
 }
