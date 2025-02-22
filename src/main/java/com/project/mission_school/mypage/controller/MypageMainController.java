780e22cb9de81e032603ba86e95719bf18fb008b
beejh02 <joon0448@naver.com>
2025-02-22 22:11:42 +0900

    [feat] 마이페이지 : 회원탈퇴 controller에 추가

--- src/main/java/com/project/mission_school/mypage/controller/MypageMainController.java
+++ src/main/java/com/project/mission_school/mypage/controller/MypageMainController.java
@@ -3,6 +3,7 @@ package com.project.mission_school.mypage.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,5 +30,20 @@ public class MypageMainController {
         MypageUser user = mypageUserService.findByUsername(username);
 
         return ResponseEntity.ok(user);
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<String> deleteUser(Authentication authentication) {
+    if (authentication == null || !authentication.isAuthenticated()) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    }
+
+    String username = authentication.getName();
+
+    MypageUser user = mypageUserService.findByUsername(username);
+
+    mypageUserService.deleteUser(user.getId());
+    
+    return ResponseEntity.ok("회원 탈퇴가 완료되었습니다.");
     }
 }
