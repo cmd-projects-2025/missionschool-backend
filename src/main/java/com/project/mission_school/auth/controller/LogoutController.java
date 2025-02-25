c6b53f260c68085e3ce92d91d38d95e03af5226e
beejh02 <joon0448@naver.com>
2025-02-25 23:19:12 +0900

    [fix] 로그아웃 : 프론트엔드에서 localstorage에서 삭제만 진행하면 되므로 수정

--- src/main/java/com/project/mission_school/auth/controller/LogoutController.java
+++ src/main/java/com/project/mission_school/auth/controller/LogoutController.java
@@ -5,23 +5,12 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletResponse;
-
 @RestController
 @RequestMapping("/api/user")
 public class LogoutController {
 
     @PutMapping("/logout")
-    public ResponseEntity<String> logout(HttpServletResponse response) {
-        Cookie jwtCookie = new Cookie("jwt", null);
-        jwtCookie.setHttpOnly(true);
-        jwtCookie.setSecure(true);
-        jwtCookie.setPath("/");
-        jwtCookie.setMaxAge(0);
-
-        response.addCookie(jwtCookie);
-
+    public ResponseEntity<String> logout() {
         return ResponseEntity.ok("로그아웃 성공");
     }
 }
