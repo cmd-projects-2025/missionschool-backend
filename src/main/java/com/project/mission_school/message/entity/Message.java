ecb5d30197cc0846eee4e170b6f2ad65e9b90eee
beejh02 <joon0448@naver.com>
2025-02-28 15:45:15 +0900

    [feat] message : entity에 NotNull과 NotBlank 추가

--- src/main/java/com/project/mission_school/message/entity/Message.java
+++ src/main/java/com/project/mission_school/message/entity/Message.java
@@ -1,12 +1,18 @@
 package com.project.mission_school.message.entity;
 
-import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -19,10 +25,13 @@ public class Message {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long messageId;
 
+    @NotNull(message = "Sender ID는 생략될 수 없습니다.")
     private Long senderId;
+    @NotNull(message = "Receiver ID는 생략될 수 없습니다.")
     private Long receiverId;
 
     @Column(length = 500)
+    @NotBlank(message = "빈 content는 허용되지 않습니다.")
     private String content;
 
     private LocalDateTime sendTime;
