3d9121617c75eb432995ade85fb591505ec04aaf
beejh02 <joon0448@naver.com>
2025-02-28 15:46:46 +0900

    [feat] message : controller에 송신시 Valid 검토 추가

--- src/main/java/com/project/mission_school/message/controller/MessageController.java
+++ src/main/java/com/project/mission_school/message/controller/MessageController.java
@@ -1,13 +1,23 @@
 package com.project.mission_school.message.controller;
 
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.project.mission_school.message.entity.Message;
 import com.project.mission_school.message.service.MessageService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/bulletin/message")
@@ -23,7 +33,7 @@ public class MessageController {
     }
 
     @PostMapping
-    public ResponseEntity<Message> sendMessage(@RequestBody Message message) {
+    public ResponseEntity<Message> sendMessage(@Valid @RequestBody Message message) {
         Message savedMessage = messageService.sendMessage(message);
         return ResponseEntity.ok(savedMessage);
     }
